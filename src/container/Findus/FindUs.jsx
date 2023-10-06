47d790d299f83f0d1c07143c03bd31e7075bf5c4
fedrick <fedrickchangs@gmail.com>
2023-10-06 17:01:02 +0700

    Add Visit Us Link

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -15,7 +15,10 @@ const FindUs = () => (
           <p className='p__opensans'>Mon - Sat: 11:00am - 14:00am | 17:00am - 22:00am</p>
           <p className='p__opensans'>Sun: 17:00am - 22:00am</p>
         </div>
-        <button className='custom__button' style={{marginTop: '2rem'}}>Visit Us</button>
+        <form action="https://www.google.com/maps/dir//Ikan+Bakar+Bulog,+Greenland,+Komplek+Ruko+Mitra+Raya,+Jl.+Ruko+Greenland+No.05-06,+Teluk+Tering,+Batam+Kota,+Batam+City,+Riau+Islands+29461/@1.124414,104.0449304,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x31d9891956982cff:0x73cfb96cedbeb80e!2m2!1d104.0475053!2d1.124414?entry=ttu" method='get' target='_blank'>
+          <button className='custom__button' style={{marginTop: '2rem'}} type='submit'>Visit Us</button>
+        </form>
+        
         
       </div>
 
